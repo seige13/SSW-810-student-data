5d8265473daf9c13860c04aab221937da4ff8df4
Chris Boffa <cboffa@its.jnj.com>
2018-08-06 20:50:00 -0400

    Add sql lite database, convert functions into class for maintainability, add headers to input files

--- HW09ChrisBoffa.py
+++ HW09ChrisBoffa.py
@@ -1,144 +1,157 @@
 from StudentClasses import Student, Instructor, Major
 from prettytable import PrettyTable
+import sqlite3
 
-students = {}
-instructors = {}
-courses = {}
-majors = {}
-passing_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']
+DB_FILE = './810_startup.db'
 
-
-def read_large_file(file_object):
-    """
-    Uses a generator to read a large file lazily
-    """
-    while True:
-        data = file_object.readline().strip()
-        if not data:
-            break
-        yield data.strip().split('\t')
+db = sqlite3.connect(DB_FILE)
 
 
-def process_students(path):
-    """Processes student from students.txt"""
-    try:
-        with open(path) as file_handler:
-            for line in read_large_file(file_handler):
-                students[line[0]] = Student(line[0], line[1], line[2])
-        return students
-    except (IOError, OSError):
-        print("Error opening or processing file: {0}".format(path))
-        exit()
+class CollegeAdministration(object):
+    passing_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']
 
+    def __init__(self, administration_name):
+        self.folder = administration_name
+        self.administration = administration_name.title()
+        self.students = {}
+        self.majors = {}
+        self.instructors = {}
+        self.grades = {}
+        self.courses = {}
+        self.read_and_process_school()
 
-def process_majors(path):
-    """Processes student from majors.txt"""
-    try:
-        with open(path) as file_handler:
-            for line in read_large_file(file_handler):
-                if not majors.get(line[0]):
-                    majors[line[0]] = Major(line[0])
+    def read_large_file(self, file_object):
+        """
+        Uses a generator to read a large file lazily
+        """
+        while True:
+            data = file_object.readline().strip()
+            if not data:
+                break
+            yield data.strip().split('\t')
 
-                if line[1].upper() == 'R':
-                    majors[line[0]].required.append(line[2])
+    def process_students(self, path):
+        """Processes student from students.txt"""
+        try:
+            with open(path) as file_handler:
+                next(file_handler)
+                for line in self.read_large_file(file_handler):
+                    self.students[line[0]] = Student(line[0], line[1], line[2])
+            return self.students
+        except (IOError, OSError):
+            print("Error opening or processing file: {0}".format(path))
+            exit()
+
+    def process_majors(self, path):
+        """Processes student from majors.txt"""
+        try:
+            with open(path) as file_handler:
+                next(file_handler)
+                for line in self.read_large_file(file_handler):
+                    if not self.majors.get(line[0]):
+                        self.majors[line[0]] = Major(line[0])
+
+                    if line[1].upper() == 'R':
+                        self.majors[line[0]].required.append(line[2])
+                    else:
+                        self.majors[line[0]].electives.append(line[1])
+
+            return self.majors
+        except (IOError, OSError):
+            print("Error opening or processing file: {0}".format(path))
+            exit()
+
+    def process_instructors(self, path):
+        """Processes instructors from instructors.txt"""
+        try:
+            with open(path) as file_handler:
+                next(file_handler)
+                for line in self.read_large_file(file_handler):
+                    self.instructors[line[0]] = Instructor(line[0], line[1], line[2])
+            return self.instructors
+        except (IOError, OSError):
+            print("Error opening or processing file: {0}".format(path))
+            exit()
+
+    def process_grades(self, path):
+        """ Assign grades to each course and process grades.txt """
+        try:
+            with open(path) as file_handler:
+                next(file_handler)
+                for line in self.read_large_file(file_handler):
+                    if self.students.get(line[0]):
+                        student = self.students.get(line[0])  # type: Student
+                        if line[2] in self.passing_grades:
+                            student.completed_courses.append(line[1])
+                    if self.instructors.get(line[3]):
+                        instructor = self.instructors.get(line[3])
+                        instructor.course.append(line[1])
+                        instructor.students += 1
+        except (IOError, OSError):
+            print("Error opening or processing file: {0}".format(path))
+            exit()
+
+    def convert_instructor_to_courses(self):
+        """ Utility function to convert instructor list to a new list identified by courses"""
+        for key, instructor in self.instructors.items():
+            for course in set(instructor.course):
+                self.courses[course] = [instructor.cwid, instructor.name, instructor.department, course,
+                                        instructor.course.count(course)]
+
+    def assign_classes(self):
+        for key, student in self.students.items():
+            if self.majors.get(student.major):
+                major = self.majors.get(student.major)  # type: Major
+                student.remaining_required = set(major.required) - set(student.completed_courses)
+                if not set(student.completed_courses).intersection(major.electives):
+                    student.remaining_electives = set(major.electives) - set(student.completed_courses)
                 else:
-                    majors[line[0]].electives.append(line[2])
+                    student.remaining_electives = None
 
-        return majors
-    except (IOError, OSError):
-        print("Error opening or processing file: {0}".format(path))
-        exit()
+    def read_and_process_school(self):
+        """ Read and process files by school self.path """
+        self.process_students('{0}/students.txt'.format(self.folder))
+        self.process_instructors('{0}/instructors.txt'.format(self.folder))
+        self.process_grades('{0}/grades.txt'.format(self.folder))
+        self.process_majors('{0}/majors.txt'.format(self.folder))
+        self.assign_classes()
 
+    def print_student_summary(self):
+        """ Print student summary table"""
+        table = PrettyTable(['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives'])
+        for key, student in self.students.items():
+            table.add_row(
+                [key, student.name, student.major, sorted(student.completed_courses), student.remaining_required,
+                 student.remaining_electives])
+        print('Student Summary')
+        print(table)
 
-def process_instructors(path):
-    """Processes instructors from instructors.txt"""
-    try:
-        with open(path) as file_handler:
-            for line in read_large_file(file_handler):
-                instructors[line[0]] = Instructor(line[0], line[1], line[2])
-        return instructors
-    except (IOError, OSError):
-        print("Error opening or processing file: {0}".format(path))
-        exit()
+    def print_major_summary(self):
+        """ Print major summary table"""
+        table = PrettyTable(['Dept', 'Required', 'Electives'])
+        for key, major in self.majors.items():
+            table.add_row([key, sorted(major.required), sorted(major.electives)])
+        print('Majors Summary')
+        print(table)
 
+    def print_instructor_summary(self, database):
+        """Print instructor summary table"""
+        cur = database.cursor()
 
-def process_grades(path):
-    """ Assign grades to each course and process grades.txt """
-    try:
-        with open(path) as file_handler:
-            for line in read_large_file(file_handler):
-                if students.get(line[0]):
-                    student = students.get(line[0])  # type: Student
-                    if line[2] in passing_grades:
-                        student.completed_courses.append(line[1])
-                if instructors.get(line[3]):
-                    instructor = instructors.get(line[3])
-                    instructor.course.append(line[1])
-                    instructor.students += 1
-    except (IOError, OSError):
-        print("Error opening or processing file: {0}".format(path))
-        exit()
+        cur.execute(
+            "select instructors.cwid, instructors.name, instructors.dept, grades.course, count(grades.student_cwid) as `students` from instructors inner join grades on instructors.cwid=grades.instructor_cwid group by grades.course order by instructors.cwid;")
+        rows = cur.fetchall()
 
-
-def convert_instructor_to_courses():
-    """ Utility function to convert instructor list to a new list identified by courses"""
-    for key, instructor in instructors.items():
-        for course in set(instructor.course):
-            courses[course] = [instructor.cwid, instructor.name, instructor.department, course,
-                               instructor.course.count(course)]
-
-
-def assign_classes():
-    for key, student in students.items():
-        if majors.get(student.major):
-            major = majors.get(student.major)  # type: Major
-            student.remaining_required = set(major.required) - set(student.completed_courses)
-            if not set(student.completed_courses).intersection(major.electives):
-                student.remaining_electives = set(major.electives) - set(student.completed_courses)
-            else:
-                student.remaining_electives = None
-
-
-def read_and_process_school(folder):
-    """ Read and process files by school folder """
-    process_students('{0}/students.txt'.format(folder))
-    process_instructors('{0}/instructors.txt'.format(folder))
-    process_grades('{0}/grades.txt'.format(folder))
-    process_majors('{0}/majors.txt'.format(folder))
-    assign_classes()
-    print_major_summary()
-    print_student_summary()
-    print_instructor_summary()
-
-
-def print_student_summary():
-    """ Print student summary table"""
-    table = PrettyTable(['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives'])
-    for key, student in students.items():
-        table.add_row([key, student.name, student.major, sorted(student.completed_courses), student.remaining_required,
-                       student.remaining_electives])
-    print('Student Summary')
-    print(table)
-
-
-def print_major_summary():
-    """ Print major summary table"""
-    table = PrettyTable(['Dept', 'Required', 'Electives'])
-    for key, major in majors.items():
-        table.add_row([key, sorted(major.required), sorted(major.electives)])
-    print('Majors Summary')
-    print(table)
-
-
-def print_instructor_summary():
-    """Print instructor summary table"""
-    convert_instructor_to_courses()
-    table = PrettyTable(['CWID', 'Name', 'Dept', 'Course', 'Students'])
-    for key, course in courses.items():
-        table.add_row([course[0], course[1], course[2], course[3], course[4]])
-    print('Instructor Summary')
-    print(table)
+        # convert_instructor_to_courses()
+        table = PrettyTable(['CWID', 'Name', 'Dept', 'Course', 'Students'])
+        for course in rows:
+            table.add_row([course[0], course[1], course[2], course[3], course[4]])
+        print('Instructor Summary')
+        print(table)
 
 
 if __name__ == '__main__':
-    read_and_process_school('stevens')
+    stevens = CollegeAdministration('stevens')
+    # print_major_summary()
+    # print_student_summary()
+    stevens.print_instructor_summary(db)
--- HW09ChrisBoffaTests.py
+++ HW09ChrisBoffaTests.py
@@ -1,20 +1,21 @@
 import unittest
-from HW09ChrisBoffa import process_students, process_instructors
+from HW09ChrisBoffa import CollegeAdministration
 
 
 class FileMethods(unittest.TestCase):
     school = 'stevens'
+    college = CollegeAdministration(school)
 
     def test_process_students(self):
         filepath = '{0}/students.txt'.format(self.school)
-        students = process_students(filepath)
+        students = self.college.process_students(filepath)
         self.assertEqual(len(students), 10)
         self.assertEqual(students.get('10103').name, 'Baldwin, C')
         self.assertEqual(students.get('10172').cwid, '10172')
 
     def test_process_instructors(self):
         filepath = '{0}/instructors.txt'.format(self.school)
-        instructors = process_instructors(filepath)
+        instructors = self.college.process_instructors(filepath)
         self.assertEqual(len(instructors), 6)
         self.assertEqual(instructors.get('98765').name, 'Einstein, A')
         self.assertEqual(instructors.get('98761').cwid, '98761')
